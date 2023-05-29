c355db3a4d3ef3a6eb58c5aa91c557e29b68eff8
@azick99 <yunusaliyevazizbek80@gmail.com>
2023-05-29 14:37:50 +0200

    from thunks to saga

---
+++ src/components/spinner/spinner.componenet.jsx
@@ -0,0 +1,10 @@
+import { SpinnerContainer, SpinnerOverlay } from './spinner';
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+);
+
+export default Spinner;
+
